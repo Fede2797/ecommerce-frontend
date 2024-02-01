005bf37b053082cde69f14071100877e514bc31d
Fede <federico2797@gmail.com>
2024-02-01 20:26:22 -0300

    Finished CartPage for desktop

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -3,10 +3,31 @@ import { Layout } from '../layout/Layout'
 import { frontUrl, productReducer, productsRoute, singleProductRoute } from '../config/constants'
 import { useAppContext } from '../AppProvider'
 import { CartProduct } from '../types/AppTypes'
+import { useEffect, useState } from 'react'
 
 export const CartPage = () => {
 
   const { state, dispatch } = useAppContext();
+  
+  const [noProductsInCart, setNoProductsInCart] = useState(true);
+  const [totalPrice, setTotalPrice] = useState(0);
+  
+  useEffect(() => {
+    if (state.length === 0) {
+      setNoProductsInCart(true);
+      return;
+    }
+    
+    const total = state.reduce(
+      (accumulator: number, product: CartProduct) => accumulator + product.price * product.quantity, 0
+    );
+
+    console.log({total});
+
+    setTotalPrice(total);
+    setNoProductsInCart(false);
+  }, [state])
+  
 
   const handleRemoveProduct = (product: CartProduct) => {
     dispatch({
@@ -74,57 +95,80 @@ export const CartPage = () => {
             <div className='px-2'>Total</div>
           </div>
 
-          {state?.map((product: CartProduct) => (
-            <div key={product._id} className="mb-5 h-[120px] grid grid-cols-6 gap-4 my-2">
-              <div className="flex col-span-3">
-                {/* Left side image */}
-                <figure className='w-[120px] h-[120px] flex justify-center items-center p-2 bg-bg-gray'>
-                  <img className='m-auto' src={product.imgSource} alt="" />
-                </figure>
-                {/* Right side info */}
-                <div className='p-2 flex flex-col gap-2'>
-                  <Link to={frontUrl + singleProductRoute + product._id}>
-                    <span className='font-poppins text-xl'>
-                      {product.name}
-                    </span>
-                  </Link>
-                  <button 
-                    className='mr-auto underline text-green'
-                    onClick={() => handleRemoveProduct(product)}
-                  >
-                    Remove
-                  </button>
+          {
+            state.map((product: CartProduct) => (
+              <div key={product._id} className="mb-5 h-[120px] grid grid-cols-6 gap-4 my-2">
+                <div className="flex col-span-3">
+                  {/* Left side image */}
+                  <figure className='w-[120px] h-[120px] flex justify-center items-center p-2 bg-bg-gray'>
+                    <img className='m-auto' src={product.imgSource} alt="" />
+                  </figure>
+                  {/* Right side info */}
+                  <div className='p-2 flex flex-col gap-2'>
+                    <Link to={frontUrl + singleProductRoute + product._id}>
+                      <span className='font-poppins text-xl'>
+                        {product.name}
+                      </span>
+                    </Link>
+                    <button 
+                      className='mr-auto underline text-green'
+                      onClick={() => handleRemoveProduct(product)}
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </div>
+                {/* Product price */}
+                <div className='p-2 flex items-center justify-end'>${product.price}</div>
+                {/* Product quantity */}
+                <div className='p-2 flex items-center justify-end'>
+                  <div className="w-[80px] h-[30px] px-1 flex justify-around items-center text-lg border-[1px] border-green">
+                    <button 
+                      className="w-full text-green"
+                      onClick={() => product.quantity > 1 && substractQuantity(product)}
+                    >
+                      -
+                    </button>
+                    <input 
+                      className="border-none w-full text-center focus:outline-none"
+                      type="text" 
+                      value={product.quantity}
+                      onChange={(event) => changeQuantity(event, product)}
+                    />
+                    <button 
+                      className="w-full text-green"
+                      onClick={() => addQuantity(product)}
+                    >
+                      +
+                    </button>
+                  </div>  
+                </div>
+                {/* Product total price */}
+                <div className='p-2 flex items-center justify-end'>${(product.price * product.quantity).toFixed(2)}</div>
               </div>
-              {/* Product price */}
-              <div className='p-2 flex items-center justify-end'>${product.price}</div>
-              {/* Product quantity */}
-              <div className='p-2 flex items-center justify-end'>
-                <div className="w-[80px] h-[30px] px-1 flex justify-around items-center text-lg border-[1px] border-green">
-                  <button 
-                    className="w-full text-green"
-                    onClick={() => product.quantity > 1 && substractQuantity(product)}
-                  >
-                    -
-                  </button>
-                  <input 
-                    className="border-none w-full text-center focus:outline-none"
-                    type="text" 
-                    value={product.quantity}
-                    onChange={(event) => changeQuantity(event, product)}
-                  />
-                  <button 
-                    className="w-full text-green"
-                    onClick={() => addQuantity(product)}
-                  >
-                    +
-                  </button>
-                </div>  
+            ))
+          }
+          {/* No products in cart */}
+          { 
+            noProductsInCart && <h1 className='my-20 text-center text-2xl font-poppins'>No products in the cart</h1>
+          }
+          <div className='mb-[140px] mt-6 pt-10 border-t-[1px] flex justify-end'>
+            {/* Subtotal */}
+            <div className='flex flex-col mr-10'>
+              <div className='w-full flex self-end justify-between items-start max-w-[200px] text-xl font-medium'>
+                <span>Sub-total </span>
+                <span>${(totalPrice).toFixed(2)}</span>
               </div>
-              {/* Product total price */}
-              <div className='p-2 flex items-center justify-end'>${product.price * product.quantity}</div>
+              <span className='mt-2 text-md text-[#9E9E9E] font-roboto font-normal'>Tax and shipping cost will be calculated later</span>
             </div>
-          ))}
+            {/* Checkout button */}
+            <button 
+              className={`w-full h-10 max-w-[180px] text-white rounded-[4px] text-xl ${noProductsInCart ? "bg-gray-400" : "bg-green"}`}
+              disabled={noProductsInCart}
+            >
+              Check-out
+            </button>
+          </div>
         </div>
       </section>
     </Layout>
