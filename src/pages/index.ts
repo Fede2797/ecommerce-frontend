d7d7e88d288659f5df396831e12157ae77dbe2fc
Fede <federico2797@gmail.com>
2024-02-02 18:41:30 -0300

    Misc (added icon and index shortcut)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,3 +5,4 @@ export { Homepage } from "./Homepage";
 export { ProductListPage } from "./ProductListPage";
 export { ProductPage } from "./ProductPage";
 export { UploadPage } from "./UploadPage";
+export { CheckOutPage } from "./CheckOutPage";
