5679ad72f6d7478cb55266b411455db56c66d33d
Fede <federico2797@gmail.com>
2024-02-05 15:36:09 -0300

    Visual fix

--- src/components/checkout/CheckoutBreadcrumbs.tsx
+++ src/components/checkout/CheckoutBreadcrumbs.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const CheckoutBreadcrumbs = () => {
   return (
-    <ul className="mb-11 flex items-center gap-3 font-medium">
+    <ul className="mb-4 flex items-center gap-3 font-medium">
       <li>Cart</li>
       <img className="h-[10px]" src="/chevron-right.svg" alt="" />
       <li>Details</li>
