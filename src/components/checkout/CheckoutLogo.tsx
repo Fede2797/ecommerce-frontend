9c0320d04b0d88b3b1298fb8a4bb28c5bfe88f6d
Fede <federico2797@gmail.com>
2024-02-05 16:45:22 -0300

    Added home Link on CheckOutPage

--- src/components/checkout/CheckoutLogo.tsx
+++ src/components/checkout/CheckoutLogo.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { frontUrl } from '../../config/constants'
 
 export const CheckoutLogo = () => {
   return (
     <div className="h-[75px] flex items-center text-xl font-bold">
-      <span className="cursor-pointer tracking-[-0.5px]">
-        Urbaneer
-      </span>
+      <Link to={frontUrl}>
+        <span className="cursor-pointer tracking-[-0.5px]">
+          Urbaneer
+        </span>
+      </Link>
     </div>
   )
 }
