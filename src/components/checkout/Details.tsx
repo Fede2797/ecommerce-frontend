e86a81b4788463789e1d97c1b602f3ca98ac1062
Fede <federico2797@gmail.com>
2024-02-05 18:18:21 -0300

    Fixed content error

--- src/components/checkout/Details.tsx
+++ src/components/checkout/Details.tsx
@@ -53,7 +53,7 @@ export const Details = () => {
             type="text" 
             id="city" 
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
-            placeholder="Name"
+            placeholder="City"
           />
           {/* Zip Code */}
           <input 
