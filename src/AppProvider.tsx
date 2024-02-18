5e0ffae9e60ec92abc17066756769777441567bf
Fede <federico2797@gmail.com>
2024-02-18 11:51:35 -0300

    Added formReducer, rooReducer and updated cartReducer. Fixed type errors

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,31 +1,57 @@
-import { createContext, useContext, useReducer, ReactNode } from 'react';
-import { productReducer } from './config/constants';
-import { CartProduct } from './types/AppTypes';
+import { createContext, useContext, useReducer, ReactNode, Dispatch } from 'react';
+import { formReducerConst, productReducer } from './config/constants';
+import { AppAction, AppState, CartState, FormState } from './types/AppTypes';
 import { addItemToCart, changeItemQuantity, removeItemFromCart } from './helpers/providerHelper';
 
-const AppContext = createContext<null | any>(null);
+const AppContext = createContext<{ state: AppState; dispatch: Dispatch<AppAction> } | null>(null);
 
 const useAppContext = () => useContext(AppContext);
 
-const initialState: CartProduct[]  = [];
+const initialCartState: CartState = {
+  cart: [],
+};
 
-const reducer = (state: CartProduct[], action: {type: string, value: CartProduct}) => {
+const cartReducer = (state: CartState, action: AppAction) => {
   switch (action.type) {
     case productReducer.ADD_PRODUCT:
-      return addItemToCart(action.value, state);
+      return { ...state, cart: addItemToCart(action.value, state.cart)};
     case productReducer.REMOVE_PRODUCT:
-      return removeItemFromCart(action.value, state);
+      return { ...state, cart: removeItemFromCart(action.value, state.cart)};
     case productReducer.CHANGE_QUANTITY:
-      return changeItemQuantity(action.value, state);
+      return { ...state, cart: changeItemQuantity(action.value, state.cart)};
     case productReducer.EMPTY_CART:
-      return [];
+      return { ...state, cart: []};
   }
   return state;
 }
 
+const initialFormState: FormState = { contact: "", first_name: "", last_name: "", city: "", shipTo: "", note: "", state: "", zipcode: "", method: "" };
+
+const formReducer = (state: FormState, action: AppAction) => {
+  switch (action.type) {
+    case formReducerConst.UPDATE_FORM:
+      return { ...state, ...action.value };
+    case formReducerConst.EMPTY_FORM:
+      return { ...initialFormState };
+  }
+  return state;
+};
+
+const initialState: AppState = {
+  cartState: initialCartState,
+  formState: initialFormState,
+};
+
+const rootReducer = (state: AppState, action: AppAction) => {
+  return {
+    cartState: cartReducer(state.cartState, action),
+    formState: formReducer(state.formState, action),
+  };
+};
+
 const AppProvider = ({ children }: { children: ReactNode }) => {
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(rootReducer, initialState);
 
   return (
     <AppContext.Provider value={{state, dispatch}}>
