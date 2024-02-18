f22256ea939de196b3f410c7bbafcbcfbca02ea3
Fede <federico2797@gmail.com>
2024-02-18 11:48:03 -0300

    Added and updated types for the AppProvider

--- src/types/AppTypes.ts
+++ src/types/AppTypes.ts
@@ -1,8 +1,10 @@
+import { formReducerConst, productReducer } from "../config/constants";
+
 export interface ProductType {
   _id: string,
   name: string,
   description: string,
-  price: string,
+  price: number,
   category: string,
   sizes: SyzeType[],
   imgSource: string,
@@ -19,7 +21,7 @@ export interface ProductUploadType {
 export interface SyzeType {
   size: number, 
   available: boolean
-};
+}
 
 export interface CartProduct {
   _id: string,
@@ -28,4 +30,60 @@ export interface CartProduct {
   size: number,
   imgSource: string,
   quantity: number,
-};
+}
+export interface CartState {
+  cart: CartProduct[];
+}
+
+export interface FormState {
+  contact: string;
+  first_name: string;
+  last_name: string;
+  city: string;
+  shipTo: string;
+  note: string;
+  state: string;
+  zipcode: string;
+  method: string;
+}
+
+export interface AppState {
+  cartState: CartState;
+  formState: FormState;
+}
+
+interface AddProductAction {
+  type: productReducer.ADD_PRODUCT;
+  value: CartProduct;
+}
+
+interface RemoveProductAction {
+  type: productReducer.REMOVE_PRODUCT;
+  value: CartProduct;
+}
+
+interface ChangeQuantityAction {
+  type: productReducer.CHANGE_QUANTITY;
+  value: CartProduct;
+}
+
+interface EmptyCartAction {
+  type: productReducer.EMPTY_CART;
+}
+
+interface UpdateFormAction {
+  type: formReducerConst.UPDATE_FORM;
+  value: Partial<FormState>;
+}
+
+interface EmptyFormAction {
+  type: formReducerConst.EMPTY_FORM;
+}
+
+export type AppAction =
+  | AddProductAction
+  | RemoveProductAction
+  | ChangeQuantityAction
+  | EmptyCartAction
+  | UpdateFormAction
+  | EmptyFormAction;
