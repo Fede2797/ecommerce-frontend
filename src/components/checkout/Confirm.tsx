98243c8380e4726d96a5f433d92de4a713ce5bcb
Fede <federico2797@gmail.com>
2024-02-18 12:07:24 -0300

    Added empty form functionality on handleEndBuy

--- src/components/checkout/Confirm.tsx
+++ src/components/checkout/Confirm.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
-import { frontUrl, productReducer } from "../../config/constants"
+import { formReducerConst, frontUrl, productReducer } from "../../config/constants"
 import { useAppContext } from "../../AppProvider"
 
 export const Confirm = () => {
 
-  const { dispatch } = useAppContext();
+  const { dispatch } = useAppContext()!;
 
   const handleEndBuy = () => {
     dispatch({ type: productReducer.EMPTY_CART });
+    dispatch({ type: formReducerConst.EMPTY_FORM });
   }
 
   return (
