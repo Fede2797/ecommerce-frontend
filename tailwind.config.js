a790eb4bdc30673685381c0c7a969b8894a585b6
Fede <federico2797@gmail.com>
2024-02-23 19:51:59 -0300

    Applied ESLint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         black: '#272727',
         green: '#56B280',
-        "bg-gray": '#F7F8FA',
-        "gray-text": '#5E6E89',
+        'bg-gray': '#F7F8FA',
+        'gray-text': '#5E6E89'
       },
       fontFamily: {
-        'poppins': "Poppins, verdana",
-        'roboto': "Roboto, verdana",
+        poppins: 'Poppins, verdana',
+        roboto: 'Roboto, verdana'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
