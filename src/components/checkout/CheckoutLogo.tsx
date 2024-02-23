d04fb374640407b09ccb2fb51f3c8d6be839012c
Fede <federico2797@gmail.com>
2024-02-23 19:57:11 -0300

    Fixed CheckoutLogo style

--- src/components/checkout/CheckoutLogo.tsx
+++ src/components/checkout/CheckoutLogo.tsx
@@ -3,8 +3,9 @@ import { checkoutSection, frontUrl } from '../../config/constants'
 
 export const CheckoutLogo = ({ section }: {section: string}) => {
   return (
-    <div className="h-[75px] flex items-center text-xl font-bold">
-      <Link to={ section !== checkoutSection.CONFIRM ? frontUrl : '#'}>
+    // For some reason it needs this div to keep the height of the inner Link to be 75px
+    <div>
+      <Link className='h-[75px] flex justify-center items-center text-xl font-bold md:justify-start' to={ section !== checkoutSection.CONFIRM ? frontUrl : '#'}>
         <span className="cursor-pointer tracking-[-0.5px]">
           Urbaneer
         </span>
