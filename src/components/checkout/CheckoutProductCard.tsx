f4eb7adef92313500d65d46e8dc50ec765db2e4b
Fede <federico2797@gmail.com>
2024-02-23 19:57:34 -0300

    Changed product card on checkout to be responsive

--- src/components/checkout/CheckoutProductCard.tsx
+++ src/components/checkout/CheckoutProductCard.tsx
@@ -2,9 +2,9 @@ import { CartProduct } from '../../types/AppTypes'
 
 export const CheckoutProductCard = ({ product }: {product: CartProduct}) => {
   return (
-    <article className="flex gap-12">
-      <figure className='relative w-[120px] h-[120px] flex justify-center items-center p-2 bg-bg-gray'>
-        <div className="absolute w-5 h-5 flex items-center justify-center bg-green top-[-5px] right-[-5px] rounded-full text-white text-center">
+    <article className="flex gap-8 md:gap-12">
+      <figure className='relative w-[90px] h-[90px] md:w-[120px] md:h-[120px] flex justify-center items-center p-2 bg-bg-gray'>
+        <div className="absolute w-4 h-4 md:w-5 md:h-5 flex items-center justify-center bg-green top-[5px] right-[0px] md:top-[-5px] md:right-[-5px] rounded-full text-white text-center text-[12px] md:text-base">
           <span>
             {product.quantity}
           </span>
