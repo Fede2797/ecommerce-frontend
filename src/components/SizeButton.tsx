70f50f2ceea9953712df98a72814c81be5c3161e
Fede <federico2797@gmail.com>
2024-02-23 19:56:20 -0300

    Re-styled SizeButtons

--- src/components/SizeButton.tsx
+++ src/components/SizeButton.tsx
@@ -1,9 +1,10 @@
 import { SyzeType } from '../types/AppTypes'
 
+// TODO: Any types
 export const SizeButton = ({ size, sizeSelected, setSizeSelected }: {size: SyzeType, sizeSelected: any, setSizeSelected:any}) => {
   return (
     <button
-      className={`w-10 h-10 flex justify-center items-center text-center rounded-[3px] border-[#D2D2D2] border-[1px] font-roboto font-normal tracking-tighter cursor-pointer duration-200
+      className={`w-11 h-11 flex justify-center items-center text-center rounded-[3px] border-[#D2D2D2] border-[1px] font-roboto font-normal tracking-tighter cursor-pointer duration-200
       ${sizeSelected === size.size
         ? 'border-none bg-[#000] text-[#FFF]'
         : ''
