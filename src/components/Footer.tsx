427ff7c7fad3aefd21033118c0e2c84b0f47f32e
Fede <federico2797@gmail.com>
2024-02-28 15:49:50 -0300

    Re-built footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,35 @@
+import { Link } from 'react-router-dom'
+import { frontUrl, kidsRoute, menRoute, productsRoute, womenRoute } from '../config/constants'
+
 export const Footer = () => {
   return (
-    <div className="w-full py-10 px-4 bg-black mt-auto">
-      <div className="w-full max-w-[1200px] min-h-[72px] mx-auto flex items-start justify-between text-white">
-        {/* Logo & description */}
-        <section className="w-full">
-          <h1 className="font-bold tracking-[-0.5px]">Urbaneer</h1>
-          <p className="max-w-[300px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, ducimus.</p>
-        </section>
-        {/* Sections */}
-        <section className="w-full flex justify-between">
-          <ul>
-            <li className="text-green font-semibold">Men</li>
-            <li>New season</li>
-            <li>Most searched</li>
-            <li>Most selled</li>
-          </ul>
-          <ul>
-            <li className="text-green font-semibold">Women</li>
-            <li>New season</li>
-            <li>Most searched</li>
-            <li>Most selled</li>
-          </ul>
-          <ul>
-            <li className="text-green font-semibold">Kids</li>
-            <li>New season</li>
-            <li>Most searched</li>
-            <li>Most selled</li>
-          </ul>
-        </section>
-      </div>
-    </div>
+    <footer className="w-full shadow bg-gray-900 text-white mt-auto">
+        <div className="w-full max-w-[1200px] mx-auto p-4 md:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <div
+                  onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse cursor-pointer"
+                >
+                  <h1 className="font-bold tracking-[-0.5px]">Urbaneer</h1>
+                </div>
+                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <li>
+                        <Link to={frontUrl + productsRoute} className="hover:underline me-4 md:me-6">Collections</Link>
+                    </li>
+                    <li>
+                        <Link to={frontUrl + menRoute} className="hover:underline me-4 md:me-6">Men</Link>
+                    </li>
+                    <li>
+                        <Link to={frontUrl + womenRoute} className="hover:underline me-4 md:me-6">Women</Link>
+                    </li>
+                    <li>
+                        <Link to={frontUrl + kidsRoute} className="hover:underline">Kids</Link>
+                    </li>
+                </ul>
+            </div>
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Urbaneer™. All Rights Reserved.</span>
+        </div>
+    </footer>
   )
 }
