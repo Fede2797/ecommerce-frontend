fa7d482ddb05c8daf7e6be1d26f18698eada33c2
Fede <federico2797@gmail.com>
2024-02-29 20:27:51 -0300

    Testing changes to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,20 @@ import { checkoutSection } from './config/constants'
 function App () {
   return (
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='https://fede2797.github.io/ecommerce-frontend/'>
         <Routes>
-          <Route path="/" Component={Homepage} />
-          <Route path="/cart" Component={CartPage} />
-          <Route path="/cart/checkout" Component={() => <CheckOutPage section={checkoutSection.DETAILS} />} />
-          <Route path="/cart/checkout/shipping" Component={() => <CheckOutPage section={checkoutSection.SHIPPING} />} />
-          <Route path="/cart/checkout/payment" Component={() => <CheckOutPage section={checkoutSection.PAYMENT} />} />
-          <Route path="/cart/checkout/payment/confirm" Component={() => <CheckOutPage section={checkoutSection.CONFIRM} />} />
-          <Route path="/products" Component={ProductListPage} />
-          <Route path="/products/men" Component={() => (<CategoryPage category="men" />)} />
-          <Route path="/products/women" Component={() => (<CategoryPage category="women" />)} />
-          <Route path="/products/kids" Component={() => (<CategoryPage category="kids" />)} />
-          <Route path="/product/:productId" Component={ProductPage} />
-          <Route path="/admin/upload" Component={UploadPage} />
+          <Route path="" Component={Homepage} />
+          <Route path="cart" Component={CartPage} />
+          <Route path="cart/checkout" Component={() => <CheckOutPage section={checkoutSection.DETAILS} />} />
+          <Route path="cart/checkout/shipping" Component={() => <CheckOutPage section={checkoutSection.SHIPPING} />} />
+          <Route path="cart/checkout/payment" Component={() => <CheckOutPage section={checkoutSection.PAYMENT} />} />
+          <Route path="cart/checkout/payment/confirm" Component={() => <CheckOutPage section={checkoutSection.CONFIRM} />} />
+          <Route path="products" Component={ProductListPage} />
+          <Route path="products/men" Component={() => (<CategoryPage category="men" />)} />
+          <Route path="products/women" Component={() => (<CategoryPage category="women" />)} />
+          <Route path="products/kids" Component={() => (<CategoryPage category="kids" />)} />
+          <Route path="product/:productId" Component={ProductPage} />
+          <Route path="admin/upload" Component={UploadPage} />
         </Routes>
       </BrowserRouter>
     </AppProvider>
